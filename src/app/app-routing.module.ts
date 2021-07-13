e9777f8c8f8b3055fd271a94325134fccdd4857d
pankajcardinal <pankaj.dwivedi@cardinalts.com>
2021-07-13 13:02:00 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardGuard } from './dashboard/dashboard.guard';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'login',
+		children: [
+			{
+				path: '',
+				pathMatch: 'full',
+				loadChildren: () => import('./authentication/login/login.module').then(l => l.LoginModule)				
+			}
+		]
+	},
+	{
+		path: 'signup',
+		children: [
+			{
+				path: '',
+				pathMatch: 'full',
+				loadChildren: () => import('./authentication/signup/signup.module').then(s => s.SignupModule)					
+			}
+		]
+	},
+	{
+		path: 'dashboard',
+		canActivate: [DashboardGuard],
+		children: [
+			{
+				path: '',
+				pathMatch: 'full',
+				loadChildren: () => import('./dashboard/dashboard.module').then(d => d.DashboardModule)					
+			}
+		]
+	},
+	{ path: '', pathMatch: 'full', component: AppComponent},
+    { path: '**', redirectTo: '/' }//default route
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgbModule,
+		DashboardModule
+	],
+	providers: [{
+    	provide: HTTP_INTERCEPTORS, 
+    	useClass: AuthInterceptor, 
+    	multi: true
+  	}],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
